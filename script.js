29c2be14c47c15a1139eaa39557afee8f78711ae
hyacinth-07 <ndr.hya37@gmail.com>
2023-02-15 13:03:44 +0100

    basic calc js logic done

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log("here I am");
+function operate (op, x, y) {
+    let result;
+    switch (op) {
+        case "sum":
+            result = add(x, y);
+            break;
+        case "sub":
+            result = subtract(x, y);
+            break;
+        case "mult":
+            result = multiply(x, y);
+            break;
+        case "div":
+            result = divide(x, y);
+            break;
+        default:
+            console.log("ERROR");
+    }
+    return result;
+}
+
+
+function add (x, y) {
+    return x + y;
+}
+
+function subtract (x, y) {
+    return x - y;
+}
+
+function multiply (x, y) {
+    return x * y
+}
+
+function divide (x, y) {
+    return x / y;
+}
+
+let a = operate("div", 5, 4)
+console.log(a);
